0aea5d0d473b0e4b89970fcb7d89886f7173be4a
thingsym <thingsym@gmail.com>
2017-07-23 01:02:58 +0900

    initial commit

---
+++ nav-menu-trim.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Nav Menu Trim
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     nav-menu-trim
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Nav_Menu_Trim
+ */
