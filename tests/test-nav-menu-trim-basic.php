044f2a58bba7f31a1f662d1ca8e127dda88a7b88
thingsym <thingsym@gmail.com>
2017-08-14 23:54:21 +0900

    fix tests

--- tests/test-nav-menu-trim-basic.php
+++ tests/test-nav-menu-trim-basic.php
@@ -12,13 +12,22 @@ class Nav_Menu_Trim_Basic_Test extends WP_UnitTestCase {
 	 * @group basic
 	 */
 	function construct() {
+		$this->assertEquals( 10, has_filter( 'init', array( $this->nav_menu_trim, 'init' ) ) );
+	}
+
+	/**
+	 * @test
+	 * @group basic
+	 */
+	function init() {
+		$this->nav_menu_trim->init();
 		$this->assertEquals( 10, has_filter( 'nav_menu_css_class', array( $this->nav_menu_trim, 'trim_css_class' ) ) );
 		$this->assertEquals( 10, has_filter( 'nav_menu_item_id', array( $this->nav_menu_trim, 'trim_item_id' ) ) );
 
 		$this->assertEquals( 10, has_action( 'customize_register', array( $this->nav_menu_trim, 'customizer' ) ) );
 		$this->assertEquals( 10, has_action( 'customize_controls_print_styles', array( $this->nav_menu_trim, 'customizer_print_styles' ) ) );
 
-		$this->assertEquals( 10, has_filter( 'plugin_action_links', array( $this->nav_menu_trim, 'plugin_action_links' ) ) );
+		$this->assertEquals( 10, has_filter( 'plugin_action_links_' . plugin_basename( __NAV_MENU_TRIM_FILE__ ), array( $this->nav_menu_trim, 'plugin_action_links' ) ) );
 	}
 
 	/**
@@ -26,12 +35,8 @@ class Nav_Menu_Trim_Basic_Test extends WP_UnitTestCase {
 	 * @group basic
 	 */
 	function customizer_print_styles() {
-		ob_start();
+		$this->expectOutputRegex( '/^<style>/' );
 		$this->nav_menu_trim->customizer_print_styles();
-		$css = ob_get_clean();
-
-		$this->assertContains( '<style>', $css );
-
 	}
 
 	/**
@@ -39,7 +44,8 @@ class Nav_Menu_Trim_Basic_Test extends WP_UnitTestCase {
 	 * @group basic
 	 */
 	function plugin_action_links() {
-		// $links = $this->nav_menu_trim->plugin_action_links( "a", "a" );
+		$links = $this->nav_menu_trim->plugin_action_links( array() );
+		$this->assertContains( 'customize.php?autofocus%5Bsection%5D=nav_menu_trim', $links[0] );
 	}
 
 }
