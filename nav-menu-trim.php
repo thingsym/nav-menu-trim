20eff57a99690374f45081fe14d0d3e9b3f0faab
thingsym <thingsym@gmail.com>
2019-05-13 20:59:09 +0900

    docs: fix header

--- nav-menu-trim.php
+++ nav-menu-trim.php
@@ -1,13 +1,15 @@
 <?php
 /**
- * Plugin Name:  Nav Menu Trim
- * Plugin URI:   https://github.com/thingsym/nav-menu-trim
- * Description:  This WordPress plugin trim html id/class attributes of Nav Menu.
- * Author:       thingsym
- * Author URI:   https://management.thingslabo.com/
- * Text Domain:  nav-menu-trim
- * Domain Path:  /languages
- * Version:      1.1.1
+ * Plugin Name: Nav Menu Trim
+ * Plugin URI:  https://github.com/thingsym/nav-menu-trim
+ * Description: This WordPress plugin trim html id/class attributes of Nav Menu.
+ * Version:     1.1.1
+ * Author:      thingsym
+ * Author URI:  https://management.thingslabo.com/
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: nav-menu-trim
+ * Domain Path: /languages
  *
  * @package      Nav_Menu_Trim
  */
